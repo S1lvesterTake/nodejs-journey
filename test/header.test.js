0387033d5854b1bc2d785c3f72cf0053739d7cfe
silvester_take <silvetake@gmail.com>
2022-04-10 19:32:53 +0800

    add travis config

--- test/header.test.js
+++ test/header.test.js
@@ -4,26 +4,27 @@ let browser, page;
 
 beforeEach(async () => {
   browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
+    args: ['--no-sandbox']
   });
 
   page = await browser.newPage();
-  await page.goto('localhost:3000');
+  await page.goto('http://localhost:3000');
 });
 
-afterEach(async()=>{
+afterEach(async () => {
   await browser.close();
-})
+});
 
 test('The header has the correct test', async () => {
   const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
   expect(text).toEqual('Blogster');
 });
 
-test('Clicking login start  oauth flow', async () =>{
+test('Clicking login start  oauth flow', async () => {
   await page.click('.right a');
 
-  const url  = await page.url()
+  const url = await page.url();
 
-  console.log(url);
-})
+  expect(url).toMatch(/accounts\.google\.com/);
+});
